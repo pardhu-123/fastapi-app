d3a6286b9493b23f4f10bc699a4a2919c801eea0
pardhu-123 <133003513+pardhu-123@users.noreply.github.com>
2025-06-20 10:48:44 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,16 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-
-DB_HOST = os.getenv("mysql.railway.internal")
-DB_PORT = os.getenv("3306")
-DB_NAME = os.getenv("railway")
-DB_USER = os.getenv("root")
-DB_PASS = os.getenv("jxVjzFSyzajoGSTzaFPibAiLibMmOETE")
-
-
-URL_DATABASE =  f"mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
