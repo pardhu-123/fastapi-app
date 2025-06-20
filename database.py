aa617509cbdd5dc8fb711cb6208b7047920fb9bb
pardhu-123 <133003513+pardhu-123@users.noreply.github.com>
2025-06-20 09:28:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-URL_DATABASE = "mysql+pymysql://root:root@localhost:3306/1000farms"
+DB_HOST = os.getenv("mysql.railway.internal")
+DB_PORT = os.getenv("3306")
+DB_NAME = os.getenv("railway")
+DB_USER = os.getenv("root")
+DB_PASS = os.getenv("jxVjzFSyzajoGSTzaFPibAiLibMmOETE")
+
+
+URL_DATABASE =  f"mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
 Base = declarative_base()
